5e1a8842b9c0f5a8c649facb21e65a3257f6672b
Santiago Saavedra <santiagosaavedra@gmail.com>
2015-12-23 15:37:41 +0100

    Add routes for everything

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,24 @@
 */
 
 Route::get('/', function () {
+    # if (Auth::check()) return 'Welcome back, ' . Auth::user()->username;
+
+    // dd(["Hi", Auth::check(), Auth::user()]);
+
+    # return 'Hi guest. ' . link_to('login', 'Login with GitHub!');
     return view('welcome');
 });
+
+Route::get('/complete/github', 'AuthController@login');
+Route::get('/login/github', 'AuthController@login');
+Route::get('/logout', 'AuthController@logout');
+
+Route::get('/profile', 'UserController@profile');
+Route::get('/profile/register', 'PaymentController@welcome');
+
+Route::get('/profile/register/{tier}', 'PaymentController@tierSelect');
+Route::post('/profile/register/free/confirm', 'PaymentController@confirmFree');
+Route::get('/profile/register/{tier}/howtopay', 'PaymentController@selectPayment');
+Route::get('/profile/register-done', 'PaymentController@confirmTier');
+Route::get('/profile/register/_callback/{merchant}', 'PaymentController@merchantCallback');
+
